35f61cf6c43ae2ed7ff5b873efa463199f9dcab6
rieg-ec <rieg@uc.cl>
2020-11-06 13:58:38 -0300

    cleanup

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ const helmet = require('helmet');
 const logger = require('morgan');
 const cors = require('cors');
 
-const middlewares = require('./middlewares');
-// const routes = require('./routes');
-
 const app = express();
 
 app.use(helmet());
@@ -25,22 +22,3 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server);
 
 require('./sockets')(io); // returns a function that accepts io object
-
-// mongoose.connect(process.env.DATABASE_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-//
-// mongoose.connection.on('connected', () => {
-//   console.log('Mongoose connected');
-// });
-//
-
-app.get('/', (req, res) => {
-  res.send('hello');
-});
-
-// middleware for 404 - not found
-app.use(middlewares.notFound);
-// last middleware, send 404 if statusCode is not found, else send 500 internal server error
-app.use(middlewares.errorHandler);
